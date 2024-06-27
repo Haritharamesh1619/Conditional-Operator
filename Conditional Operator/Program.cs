7d2cc34b32125ec38a65113edb03aa1cd0443225
Haritha R <harithalaks1998@gmail.com>
2024-06-27 15:52:30 +0530

    Add project files.

---
+++ Conditional Operator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conditional_Operator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             Console.WriteLine("Enter the student's score: ");
+            int score = int.Parse(Console.ReadLine());
+
+            string grade = score >= 90 ? "A" :
+                           score >= 80 ? "B" :
+                           score >= 70 ? "C" : 
+                           score >= 60 ? "D" : "F";
+
+            Console.WriteLine($"The student's grade is: {grade}");
+            Console.ReadLine();
+        }
+    }
+}
